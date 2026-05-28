4569faa0a683bd46de51221347d6385cbeecd61f
nyp <nyp@nyps-iMac.local>
2026-05-28 09:16:00 +0800

    experiment: concentrated_top1_3m_momentum

--- iter_2/train.py
+++ iter_2/train.py
@@ -7,39 +7,24 @@ import pandas as pd
 from prepare import format_result, run_backtest
 
 SYMBOLS = [
-    "SPY",
     "QQQ",
-    "RSP",
-    "IWM",
-    "USMV",
-    "QUAL",
-    "MTUM",
-    "VLUE",
-    "IEF",
-    "TLT",
-    "SHY",
-    "SGOV",
-    "GLD",
-    "DBC",
-    "LQD",
-    "HYG",
     "XLK",
     "XLY",
-    "XLF",
     "XLE",
-    "XLI",
-    "XLP",
-    "XLU",
-    "XLV",
-    "XLB",
-    "XLRE",
+    "MTUM",
+    "QUAL",
+    "HYG",
+    "GLD",
+    "DBC",
+    "TLT",
+    "SGOV",
 ]
-MOMENTUM_DAYS = 126
-TOP_N = 2
+MOMENTUM_DAYS = 63
+TOP_N = 1
 CASH_SYMBOL = "SGOV"
 TREND_DAYS = 0
 RISK_CAP = 1.000000000000
-USE_BLEND_SCORE = True
+USE_BLEND_SCORE = False
 USE_VOL_ADJUSTED_SCORE = False
 USE_SCORE_WEIGHTS = False
 USE_SPY_GUARD = False
